de44f64f1cb893008e677b1468f5cd3774fbe8f9
Shin Chen <xin.c_8@outlook.com>
2024-10-11 06:56:58 -0400

    updates

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -294,7 +294,7 @@ function ProductList() {
                                         <div className="product-title">{plant.name}</div>
                                         <div className='product-price'>{plant.cost}</div>
                                         <div className='product-description'>{plant.description}</div>
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        <button className="product-button" onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.name]}>{addedToCart[plant.name] ? "Added" : "Add to Cart"}</button>
                                     </div>
                                 ))}
                             </div>
